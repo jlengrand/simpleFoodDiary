84d033f190a61d94a41093c268d7ba47468c1302
Julien Lengrand-Lambert <julien@lengrand.fr>
2020-02-10 22:30:33 +0100

    Basic login working

--- src/Main.elm
+++ src/Main.elm
@@ -1,20 +1,50 @@
-module Main exposing (..)
+port module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
+import Html exposing (Html, button, div, h1, h2, img, text)
 import Html.Attributes exposing (src)
+import Html.Events exposing (onClick)
+import Json.Decode
+import Json.Decode.Pipeline
+import Json.Encode
+
 
 
 ---- MODEL ----
 
 
+port signIn : () -> Cmd msg
+
+
+port signInInfo : (Json.Encode.Value -> msg) -> Sub msg
+
+
+port signInError : (Json.Encode.Value -> msg) -> Sub msg
+
+
+port signOut : () -> Cmd msg
+
+
+type alias ErrorData =
+    { code : Maybe String, message : Maybe String, credential : Maybe String }
+
+
+type alias UserData =
+    { token : String, email : String, uid : String }
+
+
 type alias Model =
-    {}
+    { userData : Maybe UserData, error : ErrorData }
+
+
+emptyError : ErrorData
+emptyError =
+    { code = Maybe.Nothing, credential = Maybe.Nothing, message = Maybe.Nothing }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { userData = Maybe.Nothing, error = emptyError }, Cmd.none )
 
 
 
@@ -22,16 +52,50 @@ init =
 
 
 type Msg
-    = NoOp
+    = LogIn
+    | LogOut
+    | LoggedInData (Result Json.Decode.Error UserData)
+    | LoggedInError (Result Json.Decode.Error ErrorData)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        LogIn ->
+            ( model, signIn () )
+
+        LogOut ->
+            ( { model | userData = Maybe.Nothing, error = emptyError }, signOut () )
+
+        LoggedInData result ->
+            case result of
+                Ok value ->
+                    ( { model | userData = Just value }, Cmd.none )
+
+                Err error ->
+                    ( { model | error = messageToError <| Json.Decode.errorToString error }, Cmd.none )
+
+        LoggedInError result ->
+            case result of
+                Ok value ->
+                    ( { model | error = value }, Cmd.none )
+
+                Err error ->
+                    ( { model | error = messageToError <| Json.Decode.errorToString error }, Cmd.none )
 
 
 
 ---- VIEW ----
+
+
+messageToError : String -> ErrorData
+messageToError message =
+    { code = Maybe.Nothing, credential = Maybe.Nothing, message = Just message }
+
+
+errorPrinter : ErrorData -> String
+errorPrinter errorData =
+    Maybe.withDefault "" errorData.code ++ " " ++ Maybe.withDefault "" errorData.credential ++ " " ++ Maybe.withDefault "" errorData.message
 
 
 view : Model -> Html Msg
@@ -39,11 +103,51 @@ view model =
     div []
         [ img [ src "/logo.svg" ] []
         , h1 [] [ text "Your Elm App is working!" ]
+        , case model.userData of
+            Just data ->
+                button [ onClick LogOut ] [ text "Logout from Google" ]
+
+            Maybe.Nothing ->
+                button [ onClick LogIn ] [ text "Login with Google" ]
+        , h2 []
+            [ text <|
+                case model.userData of
+                    Just data ->
+                        data.email ++ " " ++ data.uid ++ " " ++ data.token
+
+                    Maybe.Nothing ->
+                        ""
+            ]
+        , h2 [] [ text <| errorPrinter model.error ]
         ]
 
 
 
 ---- PROGRAM ----
+
+
+userDataDecoder : Json.Decode.Decoder UserData
+userDataDecoder =
+    Json.Decode.succeed UserData
+        |> Json.Decode.Pipeline.required "token" Json.Decode.string
+        |> Json.Decode.Pipeline.required "email" Json.Decode.string
+        |> Json.Decode.Pipeline.required "uid" Json.Decode.string
+
+
+logInErrorDecoder : Json.Decode.Decoder ErrorData
+logInErrorDecoder =
+    Json.Decode.succeed ErrorData
+        |> Json.Decode.Pipeline.required "code" (Json.Decode.nullable Json.Decode.string)
+        |> Json.Decode.Pipeline.required "message" (Json.Decode.nullable Json.Decode.string)
+        |> Json.Decode.Pipeline.required "credential" (Json.Decode.nullable Json.Decode.string)
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch
+        [ signInInfo (Json.Decode.decodeValue userDataDecoder >> LoggedInData)
+        , signInError (Json.Decode.decodeValue logInErrorDecoder >> LoggedInError)
+        ]
 
 
 main : Program () Model Msg
@@ -52,5 +156,5 @@ main =
         { view = view
         , init = \_ -> init
         , update = update
-        , subscriptions = always Sub.none
+        , subscriptions = subscriptions
         }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,74 @@
 import './main.css';
 import { Elm } from './Main.elm';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
-Elm.Main.init({
+const firebaseConfig = {
+  apiKey: process.env.ELM_APP_API_KEY,
+  authDomain: process.env.ELM_APP_AUTH_DOMAIN,
+  databaseURL: process.env.ELM_APP_DATABASE_URL,
+  projectId: process.env.ELM_APP_PROJECT_ID,
+  storageBucket: process.env.ELM_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.ELM_APP_MESSAGING_SENDER_ID,
+  appId: process.env.ELM_APP_APP_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const provider = new firebase.auth.GoogleAuthProvider();
+const db = firebase.firestore();
+
+const app = Elm.Main.init({
   node: document.getElementById('root')
+});
+
+app.ports.signIn.subscribe(() => {
+  console.log("LogIn called");
+  firebase
+    .auth()
+    .signInWithRedirect(provider)
+    .then(result => {
+      result.user.getIdToken().then(idToken => {
+        app.ports.signInInfo.send({
+          token: idToken,
+          email: result.user.email,
+          uid: result.user.uid
+        });
+      });
+    })
+    .catch(error => {
+      app.ports.signInError.send({
+        code: error.code,
+        message: error.message
+      });
+    });
+});
+
+app.ports.signOut.subscribe(() => {
+  console.log("LogOut called");
+  firebase.auth().signOut();
+});
+
+//  Observer on user info
+firebase.auth().onAuthStateChanged(user => {
+  console.log("called");
+  if (user) {
+    user
+      .getIdToken()
+      .then(idToken => {
+        app.ports.signInInfo.send({
+          token: idToken,
+          email: user.email,
+          uid: user.uid
+        });
+      })
+      .catch(error => {
+        console.log("Error when retrieving cached user");
+        console.log(error);
+      });
+  }
 });
 
 // If you want your app to work offline and load faster, you can change
